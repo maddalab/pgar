2c1036748a09a7b58e81868cdcbede56e8a24ff5
Bhaskar Maddala <maddalab@gmail.com>
2021-07-12 19:34:44 +0530

    installing docker is more involved now.

--- user_data.sh
+++ user_data.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 set -e
 
+# install docker - instructions are at https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
+apt install apt-transport-https ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt install docker-ce docker-ce-cli containerd.io
+
 # install necessary tools
-apt install -y curl jq docker.io
+apt install -y git jq
 
 # create a user and group for github runners
 addgroup runner
